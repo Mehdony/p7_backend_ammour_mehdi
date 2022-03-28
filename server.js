28e09c202eb28113bd8967d7efc5545a8653363b
Mehdony <86969900+Mehdony@users.noreply.github.com>
2022-03-28 14:14:12 +0200

    Affichage des posts OK

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
@@ -29,5 +30,5 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
-
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use("/api/auth", userRoutes);
